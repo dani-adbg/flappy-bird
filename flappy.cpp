d092086ca741d2b804b2f5a5f868aaeb318fd157
dani-adbg <113252926+dani-adbg@users.noreply.github.com>
2023-08-20 23:20:27 -0500

    Update flappy.cpp

--- flappy.cpp
+++ flappy.cpp
@@ -8,18 +8,18 @@
 
 using namespace std;
 
+const int screenWidth = 115;
+const int winWidth = 95;
+const int screenHeight = 25;
+const int obstacleSpacing = 25; 
+int score = 0;
+
 class Bird
 {
 public:
-  int x;
   int y;
 
-  Bird() : x(10), y(11) {}
-
-  int getCoordX() const
-  {
-    return x;
-  }
+  Bird() : y(screenHeight / 2) {}
 
   int getCoordY() const
   {
@@ -53,71 +53,80 @@ public:
 
 vector<Obstacle> obstacles;
 
-const int screenWidth = 100;
-const int screenHeight = 25;
-char screen[screenHeight][screenWidth];
-const int obstacleSpacing = 25;  // Espacio entre obstáculos
-
-// void gotoxy(int x, int y) {
-//   HANDLE hCon;
-//   hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-//   COORD dwPos;
-//   dwPos.X = x;
-//   dwPos.Y = y;
-
-//   SetConsoleCursorPosition(hCon, dwPos);
-// }
-
-void generateObstacle() {
-    int gapY = rand() % (screenHeight - 10) + 5;  // Generar altura del espacio entre los tubos
-    obstacles.emplace_back(screenWidth - 1, gapY, 6);  // Agregar obstáculo al final de la lista
-}
-
-void updateObstacles() {
-    for (size_t i = 0; i < obstacles.size(); i++) {
-        obstacles[i].x--;
-
-        // Eliminar obstáculos que salen de la pantalla
-        if (obstacles[i].x + obstacles[i].width < 0) {
-            obstacles.erase(obstacles.begin() + i);
-            i--;
-        }
-    }
-}
-
-void drawObstacles() {
-    for (const Obstacle& obstacle : obstacles) {
-        for (int i = 0; i < screenHeight; i++) {
-            if (i < obstacle.gapY || i > obstacle.gapY + 8) {
-                screen[i][obstacle.x] = '|';
-                screen[i][obstacle.x + 1] = '|';
-            }
-        }
-    }
-}
-
-bool checkCollision(Bird& bird) {
-    for (const Obstacle& obstacle : obstacles) {
-        if (bird.getCoordX() + 4 >= obstacle.x && bird.getCoordX() <= obstacle.x + obstacle.width) {
-            if (bird.getCoordY() <= obstacle.gapY || bird.getCoordY() + 1 >= obstacle.gapY + 8) {
-                return true;  // Colisión detectada
-            }
-        }
-    }
-    return false;  // No hay colisión
-}
-
-void clearScreen()
+void gotoxy(int x, int y)
 {
-  system("cls"); // Limpiar la consola
-  // Dibujar borde con #
-  for (int i = 0; i < screenHeight; i++)
+  HANDLE hCon;
+  hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD dwPos;
+  dwPos.X = x;
+  dwPos.Y = y;
+
+  SetConsoleCursorPosition(hCon, dwPos);
+}
+
+void generateObstacle()
+{
+  int gapY = rand() % (screenHeight - 10) + 5;   // Generar altura del espacio entre los tubos
+  obstacles.emplace_back(winWidth - 3, gapY, 6); // Agregar obstáculo al final de la lista
+}
+
+void updateObstacles()
+{
+  for (size_t i = 0; i < obstacles.size(); i++)
   {
-    for (int j = 0; j < screenWidth; j++)
+    obstacles[i].x--;
+
+    if (obstacles[i].x == 1)
     {
-      screen[i][j] = ' ';
+      obstacles.erase(obstacles.begin() + i);
+      i--;
     }
   }
+}
+
+void drawObstacles()
+{
+  for (const Obstacle &obstacle : obstacles)
+  {
+    for (int i = 1; i < screenHeight; i++)
+    {
+      if (i < obstacle.gapY || i > obstacle.gapY + 8)
+      {
+        gotoxy(obstacle.x, i);
+        cout << "|||";
+      }
+    }
+  }
+}
+void eraseObstacles()
+{
+  for (const Obstacle &obstacle : obstacles)
+  {
+    for (int i = 1; i < screenHeight; i++)
+    {
+      if (i < obstacle.gapY || i > obstacle.gapY + 8)
+      {
+        gotoxy(obstacle.x, i);
+        cout << "   ";
+      }
+    }
+  }
+}
+
+bool collision(Bird &bird)
+{
+  int birdX = 11;
+  for (const Obstacle &obstacle : obstacles)
+  {
+    if (birdX >= obstacle.x && birdX <= obstacle.x + obstacle.width)
+    {
+      if (bird.getCoordY() <= obstacle.gapY || bird.getCoordY() + 1 >= obstacle.gapY + 8)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 void drawBorder()
@@ -125,36 +134,24 @@ void drawBorder()
   // Dibujar borde con #
   for (int i = 0; i < screenWidth; i++)
   {
-    screen[0][i] = '#';
-    screen[screenHeight - 1][i] = '#';
+    gotoxy(i, 0);
+    cout << "#";
+    gotoxy(i, screenHeight);
+    cout << "#";
   }
-  for (int i = 1; i < screenHeight - 1; i++)
-  {
-    screen[i][0] = '#';
-    screen[i][screenWidth - 1] = '#';
-  }
-}
 
-void updateScreen(Bird &bird)
-{
-  // Dibujar el pájaro en la posición actual
-  int birdX = bird.getCoordX();
-  int birdY = bird.getCoordY();
-  if (birdY >= 0 && birdY < screenHeight - 1)
+  for (int i = 0; i < screenHeight; i++)
   {
-    screen[birdY][birdX] = '/';
-    screen[birdY][birdX + 1] = '-';
-    screen[birdY][birdX + 2] = '-';
-    screen[birdY][birdX + 3] = '0';
-    screen[birdY][birdX + 4] = '\\';
-    if (birdY + 1 < screenHeight - 1)
-    {
-      screen[birdY + 1][birdX] = '|';
-      screen[birdY + 1][birdX + 1] = 'u';
-      screen[birdY + 1][birdX + 2] = 'u';
-      screen[birdY + 1][birdX + 3] = '_';
-      screen[birdY + 1][birdX + 4] = '>';
-    }
+    gotoxy(0, i);
+    cout << "#";
+    gotoxy(screenWidth, i);
+    cout << "#";
+  }
+
+  for (int i = 0; i < screenHeight; i++)
+  {
+    gotoxy(winWidth, i);
+    cout << "#";
   }
 }
 
@@ -167,78 +164,131 @@ void cursor(bool game)
   SetConsoleCursorInfo(hConsole, &cursorInfo);
 }
 
-void showGameOver()
+void drawBird(Bird &bird)
+{
+  int birdY = bird.getCoordY();
+
+  gotoxy(5, birdY - 1);
+  cout << "/--0\\";
+  gotoxy(5, birdY);
+  cout << "|uu__>";
+}
+void eraseBird(Bird &bird)
+{
+  int birdY = bird.getCoordY();
+
+  gotoxy(5, birdY - 1);
+  cout << "     ";
+  gotoxy(5, birdY);
+  cout << "      ";
+}
+
+void gameOver()
 {
   system("cls");
-  cout << "Game Over" << endl;
-  cursor(true);
+  gotoxy((screenWidth / 2) - 25, (screenHeight / 2) - 1);
+  cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+  gotoxy(screenWidth / 2, screenHeight / 2);
+  cout << "Game Over";
+  gotoxy((screenWidth / 2) - 1, screenHeight / 2);
+  cout << "Puntaje: " << score;
+  gotoxy((screenWidth / 2) - 25, (screenHeight / 2) + 1);
+  cout << "Presiona ESPACIO para jugar otra vez o ENTER para salir...";
+  gotoxy((screenWidth / 2) - 25, (screenHeight / 2) + 2);
+  cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+}
+
+bool scoreCollision()
+{
+  int birdX = 9;
+  for (const Obstacle &obstacle : obstacles)
+  {
+    if (birdX == obstacle.x)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+void updateScore() {
+  gotoxy(winWidth + 6, 5);
+  cout << "Puntos: " << score << endl; 
 }
 
 int main()
 {
-  Bird bird;
-  auto startTime = chrono::steady_clock::now();
-  bool gameOver = false;
+  bool playAgain = true;
 
-  cursor(gameOver);
-
-  while (!gameOver)
+  while (playAgain)
   {
-    if (_kbhit())
-    {
-      char key = _getch();
-      if (key == ' ')
-      {
-        bird.moveUp();
-      }
-    }
-
-    if (bird.getCoordY() == 0 || bird.getCoordY() == screenHeight - 2)
-    {
-      showGameOver();
-      gameOver = true; // Cambiar el estado del juego para salir del bucle
-    }
-
-    auto currentTime = chrono::steady_clock::now();
-    auto elapsedTime = chrono::duration_cast<chrono::milliseconds>(currentTime - startTime).count();
-
-    if (elapsedTime >= 100 && !gameOver)
-    { // Evitar mover el pájaro si el juego ha terminado
-      bird.moveDown();
-      updateObstacles();
-      if (obstacles.empty() || screenWidth - obstacles.back().x >= obstacleSpacing) {
-                generateObstacle();
-      }
-      startTime = currentTime;
-    }
-
-    clearScreen();
+    Bird bird;
+    cursor(false);
     drawBorder();
-    if (!gameOver)
-    {
-      updateScreen(bird);
-      drawObstacles();
-    }
+    obstacles.clear();
+    bool game = true;
 
-    if (checkCollision(bird)) {
-            showGameOver();
-            gameOver = true;
-        }
+    updateScore();
 
-    // Imprimir el contenido actualizado del recuadro
-    for (int i = 0; i < screenHeight; i++)
+    gotoxy(winWidth + 5, 2);cout<<"FLAPPY BIRD";
+	  gotoxy(winWidth + 5, 4);cout<<"-----------";
+	  gotoxy(winWidth + 5, 6);cout<<"-----------";
+	  gotoxy(winWidth + 7, 12);cout<<"Control ";
+	  gotoxy(winWidth + 6, 13);cout<<"---------";
+	  gotoxy(winWidth + 2, 14);cout<<" Espacio = Salta";
+
+    while (game)
     {
-      for (int j = 0; j < screenWidth; j++)
+      if (_kbhit())
       {
-        cout << screen[i][j];
+        char key = _getch();
+        if (key == ' ')
+        {
+          bird.moveUp();
+        }
       }
-      cout << endl;
+
+      drawBird(bird);
+
+      int birdY = bird.getCoordY();
+
+      if (birdY == screenHeight || birdY - 1 == 0 || collision(bird) == true)
+      {
+        game = false;
+      }
+      updateObstacles();
+
+      if (obstacles.empty() || winWidth - obstacles.back().x >= obstacleSpacing)
+      {
+        generateObstacle();
+      }
+      drawObstacles();
+
+      Sleep(100);
+      eraseBird(bird);
+      eraseObstacles();
+      bird.moveDown();
+
+      if(scoreCollision() == true) {
+        score++;
+        updateScore();
+      }
+
     }
 
-    Sleep(100);
+    gameOver();
+    score = 0;
+    Sleep(1000);
+    char key;
+    do
+    {
+      key = _getch();
+    } while (key != ' ' && key != 13); 
+    if (key == 13)
+    {
+      playAgain = false;
+    }
+    system("cls"); 
   }
-
-  showGameOver();
-
-  getch();
+  return 0;
 }
